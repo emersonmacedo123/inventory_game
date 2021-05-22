47d7c2adae5e1fd6950edef36f8c3e61dfa22347
Emerson Raniere de Macedo <56753061+emersonmacedo123@users.noreply.github.com>
2021-05-22 17:04:56 +0200

    Print table implemented (missing sorting)

--- game_inventory.py
+++ game_inventory.py
@@ -17,6 +17,8 @@ def display_inventory(inventory):
     for key, value in inventory.items():
         print(f"{key}: {value}")
 
+
+
 def add_to_inventory(inventory, added_items):
     """Add to the inventory dictionary a list of items from added_items."""
     for item in added_items:
@@ -25,6 +27,8 @@ def add_to_inventory(inventory, added_items):
         else:
             inventory[item] = 1
     return inventory
+
+
 
 def remove_from_inventory(inventory, removed_items):
     """Remove from the inventory dictionary a list of items from removed_items."""
@@ -35,18 +39,20 @@ def remove_from_inventory(inventory, removed_items):
                 inventory.pop(item)
     return inventory
 
-list = ["Key9"]
-remove_from_inventory(myInventory, list)
-display_inventory(myInventory)
 
 def print_table(inventory, order):
     """
     Display the contents of the inventory in an ordered, well-organized table with
     each column right-aligned.
     """
+    print("-----------------")
+    print("item name | count")
+    print("-----------------")
+    for item in inventory.keys():
+        print(f"     {item} |  {inventory[item]}")
+    print("-----------------")
 
-    pass
-
+print_table(myInventory,1)
 
 def import_inventory(inventory, filename):
     """Import new inventory items from a CSV file."""
